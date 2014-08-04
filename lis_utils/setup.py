935deca7fd716bf0b9a268223eb34f671020e553
mukolx <mukolx@yahoo.com>
2014-08-04 17:59:47 +0200

    added  subprocess command

--- lis_utils/setup.py
+++ lis_utils/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='simple_lis2d_utils',
-    version='0.1.0',
+    version='0.1.1',
     author='Micah Mukolwe',
     author_email='mukolx@mail.com',
     packages=['lis_utils'],
