a140d74ba5c795eaa9c93e9932855c6b04bbcc0e
mukolx <mukol1@1202HPL-003.education.ihe.nl>
2014-08-06 15:54:58 +0200

    file changes

--- lis_utils/lis_utils/run_system_command.py
+++ lis_utils/lis_utils/run_system_command.py
@@ -1,18 +1,13 @@
 import subprocess
 import os
 
-def run_command(commnd):
-    process = subprocess.Popen(commnd,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE,
-                               shell=True)
-    output, errors = process.communicate()
-    #checking run status
-    if process.returncode != 0:
+def run_sys_command(comman2run):
+    procs = subprocess.Popen(comman2run,stdout=subprocess.PIPE,\
+                             stderr=subprocess.PIPE,shell=True)
+    output, errors = procs.communicate()
+    if procs.returncode != 0:
         sys.stderr.write(errors)
         sys.exit(1)
     else:
         sys.stdout.write(output)
-    ### incase the subprocess command fails use 
-    ### os.system(cmmnd)
     return output
